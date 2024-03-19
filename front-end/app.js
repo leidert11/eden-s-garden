50fe68182cd4638ffdfd003bf777d35bab2f3521
leidert11 <ltami216@gmail.com>
2024-03-19 13:22:23 -0500

    feat: :sparkles: consumes some queries in the console

--- front-end/app.js
+++ front-end/app.js
@@ -15,12 +15,35 @@ import * as product from './js/product.js';
 
 // app.js
 
+// 1
 
 document.getElementById("customer1").addEventListener("click", function() {
     customer.spanishCustomers();
 });
 
+// 2
 document.getElementById("customer2").addEventListener("click", function() {
+    customer.madridSalesRep11To30();
+});
+// 3
+document.getElementById("customer3").addEventListener("click", function() {
     customer.customerSalesRep();
 });
 
+//4
+document.getElementById("customer4").addEventListener("click", function() {
+    customer.customersWithPayments();
+});
+
+//5
+document.getElementById("customer5").addEventListener("click", function() {
+    customer.customersWithoutPayments();
+});
+//6
+document.getElementById("customer6").addEventListener("click", function() {
+    customer.customersWithPaymentsRepCity();
+});
+//7
+document.getElementById("customer7").addEventListener("click", function() {
+    customer.withoutPayments();
+});
--- front-end/js/customer.js
+++ front-end/js/customer.js
@@ -1,4 +1,5 @@
 
+// 1
 
 export function spanishCustomers() {
     const token = sessionStorage.getItem('jwtToken'); 
@@ -15,9 +16,85 @@ export function spanishCustomers() {
     .catch(error => console.error('Error:', error));
 }
 
+// 2
+export function madridSalesRep11To30() {
+    const token = sessionStorage.getItem('jwtToken');
+    fetch('http://localhost:8080/api/customers/madrid-sales-rep-11-30', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 3
 export function customerSalesRep() {
     const token = sessionStorage.getItem('jwtToken'); 
     fetch('http://localhost:8080/api/customers/customer-salesrep', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+//4
+export function customersWithPayments() {
+    const token = sessionStorage.getItem('jwtToken');
+    fetch('http://localhost:8080/api/customers/customers-with-payments', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+//5
+export function customersWithoutPayments() {
+    const token = sessionStorage.getItem('jwtToken');
+    fetch('http://localhost:8080/api/customers/customers-without-payments', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+//6
+export function customersWithPaymentsRepCity() {
+    const token = sessionStorage.getItem('jwtToken');
+    fetch('http://localhost:8080/api/customers/customers-with-payments-rep-city', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+//7
+export function withoutPayments() {
+    const token = sessionStorage.getItem('jwtToken');
+    fetch('http://localhost:8080/api/customers/without-payments', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${token}`
