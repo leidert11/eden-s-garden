b920b6f1253ac7d22af92a749777c2be074f3d28
Ancreem <ancreemroyal@gmail.com>
2024-03-19 18:19:08 -0500

    feat: :sparkles: consumption in the office and employee console

--- front-end/js/employee.js
+++ front-end/js/employee.js
@@ -1,8 +1,9 @@
 
+// 1
 
 export function employeesByBossId7() {
     const token = sessionStorage.getItem('jwtToken'); 
-    fetch('http://localhost:8080/api/employee/employees-by-boss-id-7', {
+    fetch('http://localhost:8080/employee/employees-by-boss-id-7', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${token}`
@@ -14,3 +15,170 @@ export function employeesByBossId7() {
     })
     .catch(error => console.error('Error:', error));
 }
+
+// 2
+export function getBossOfCompany() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/boss-of-company', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 3
+export function findEmployeesNotSalesRepresentatives() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/employees-not-sales-representatives', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 4
+export function getEmployeeAndBossNames() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/employee-and-boss-names', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 5
+export function getAllEmployeesWithBossAndGrandBoss() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/with-boss-and-grandboss', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 6
+export function getEmployeesNotAssociatedOffice() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/employees-not-associated-office', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 7
+export function getNonClientOfficeEmployeesWork() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/non-client-office-employees-work', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 8
+export function getEmployeesDoNotHaveClients() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/employees-do-not-have-clients', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 9
+export function getEmployeesNotAssociatedOfficeAndNotClient() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/employees-not-associated-office-and-not-client', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 10
+export function findEmployeesWithoutCustomers() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/find-employees-without-customers', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 11
+export function countEmployees() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/count-employees', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 12
+export function countCustomersByEmployee() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/employee/count-customers-by-employee', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
--- front-end/js/office.js
+++ front-end/js/office.js
@@ -1,1 +1,75 @@
-const API_URL = 'http://localhost:8080/jardineria';
+// 1
+export function getOfficeCodeAndCity() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/office/office-code-and-city', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 2
+export function getCityAndPhoneInSpain() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/office/city-and-phone-in-spain', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 3
+export function findCustomersWithPaymentsAndTheirSalesRepAndOfficeCity() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/office/get-customers-with-payments-and-their-sales-reps-and-office-city', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 4
+export function findOfficeAddressesInFuenlabrada() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/office/fuenlabrada-office-addresses', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
+// 5
+export function getOfficesNotEmployeesHaveClientWithGamaFrutales() {
+    const token = sessionStorage.getItem('jwtToken'); 
+    fetch('http://localhost:8080/office/ofices-not-employees-have-client-with-gama-frutales', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
